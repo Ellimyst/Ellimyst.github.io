e44ee48f928e4af20a251a7c5a2882b1a9c9e672
Ellimyst <sumukhvaze@gmail.com>
2023-02-08 21:00:52 -0800

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Navigation/Navbar.js";
+import Navbar from "../Navigation/Navbar.js";
 
 function App() {
   return (
