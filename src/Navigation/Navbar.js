ab5213e85060a4ff5d3ca99f50a747443059ca02
Ellimyst <sumukhvaze@gmail.com>
2023-02-13 21:05:31 -0800

    color for every page

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({changeBg}) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">
@@ -22,10 +22,10 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => changeBg("#99f7e0")}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/shrine">Shrine</Link>
+            <Link to="/shrine" onClick={() => changeBg("#416070")}>Shrine</Link>
           </li>
         </ul>
       </div>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../Navigation/Navbar.js";
 import Main from '../Main.js'
 
 function App() {
+  const [color, changeColor] = React.useState("#99f7e0");
+  document.body.style.backgroundColor = color;
+
+  const changeBg = (bg) => {
+    changeColor(bg)
+  }
+
   return (
     <div className="App">
       <div>
-        <Navbar />
+        <Navbar changeBg={changeBg} />
       </div>
       <div>
         <Main />
@@ -15,5 +23,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
