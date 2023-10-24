9f82dfa5a66978d79b0975cc027a8dcc382fcaa1
Ellimyst <sumukhvaze@gmail.com>
2023-10-24 15:02:37 -0700

    update end date

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -14,7 +14,7 @@ const CalendarPage = ({checkAuth}) => {
   
   // Set your startDate and endDate here
   const startDateStr = '2023-06-22';
-  const endDateStr = '2023-12-15';
+  const endDateStr = '2023-12-05';
   const startDate = moment(startDateStr);
   const endDate = moment(endDateStr);
 
